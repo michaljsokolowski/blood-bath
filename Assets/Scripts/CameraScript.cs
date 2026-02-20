bd728b82a39c0553e8d3a53f560a0a1361ed474e
świerk <mystique.exarr@gmail.com>
2026-02-20 16:13:04 +0100

    ściany
    -> ściany dookoła terenu, które blokują kamerę i movement gracza, żeby nie dało się wyjść out of bounds
    -> enemy fixy od szymona

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -9,18 +9,40 @@ public class CameraScript : MonoBehaviour
         [SerializeField] private Transform target;
         [SerializeField] private float smoothTime;
         private Vector3 _currentVelocity = Vector3.zero;
+        private Bounds _wallBounds;
         
     #endregion
     
     #region Unity callbacks
     
-        private void Awake() => _offset = transform.position - target.position;
+        private void Awake()
+        {
+            _offset = transform.position - target.position;
+            CalculateWallBounds();
+        }
 
         private void LateUpdate()
         {
             Vector3 targetPosition = target.position + _offset;
+            
+            targetPosition.x = Mathf.Clamp(targetPosition.x, _wallBounds.min.x, _wallBounds.max.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, _wallBounds.min.y, _wallBounds.max.y);
+            targetPosition.z = Mathf.Clamp(targetPosition.z, _wallBounds.min.z, _wallBounds.max.z);
+            
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, smoothTime);
         }
         
     #endregion
+    
+    private void CalculateWallBounds()
+    {
+        GameObject wallObject = GameObject.FindWithTag("Wall");
+
+        BoxCollider[] colliders = wallObject.GetComponents<BoxCollider>();
+        _wallBounds = colliders[0].bounds;
+        for (int i = 1; i < colliders.Length; i++)
+        {
+            _wallBounds.Encapsulate(colliders[i].bounds);
+        }
+    }
 }
--- Assets/Scripts/enemyScripts/EnemyScript.cs
+++ Assets/Scripts/enemyScripts/EnemyScript.cs
@@ -91,7 +91,10 @@ public class EnemyScript : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, fireRange);
     }
-
+    public void EnemyReceiveHit(int damage)
+    {
+        TakeDamage(damage);
+    }
     public void TakeDamage(int damage)
     {
         currentHealth -= damage;
