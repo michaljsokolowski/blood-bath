92612855ff5dfe75e3c71bf7f5880322715007c9
DagonGarinCel <michal.jan.g.97@gmail.com>
2026-03-22 13:52:21 +0100

    TimeTrap&NavmeshKnowlege
    Płapka zadająca obrażenia po upływie czasu przez jakiś czas i by nav mesh działał odpowiednio po zniszczeniu obiektu do owego trzeba dodać NavMeshObstacle z odpowiednimi ustawieniami

---
+++ Assets/Scripts/Trap/TimeTrap.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeTrap : MonoBehaviour
+{
+   [SerializeField]
+    float timeTo = 5f;
+   [SerializeField]
+    Text displayTimer;
+    [SerializeField]
+    float remaningTime;
+    [SerializeField]
+    GameObject textObject;
+
+    public int trapDamage = 10;
+
+    private void Start()
+    {
+        remaningTime = timeTo;
+        textObject.SetActive(false);
+
+    }
+    public void OnTriggerEnter(Collider collision)
+    {
+
+        if (collision.CompareTag("Player") && collision is BoxCollider)
+        {
+            
+           StartCoroutine(TimerToDamage(1f));
+            var combat = collision.GetComponent<CombatScript>();
+            if (timeTo <= 0)
+            {
+                combat.TakeDamage(trapDamage, null);
+            }
+
+        }
+        StartCoroutine(resetTimer(10f));
+    }
+    private void Update()
+    {
+        displayTimer.text = timeTo.ToString("0");
+    }
+    IEnumerator TimerToDamage(float time)
+    {
+        textObject.SetActive(true);
+        while (remaningTime > 0)
+        {
+            yield return new WaitForSeconds(time);
+            timeTo--;
+            updateTime();
+            
+        }
+        countDownFinished();
+       
+    }
+
+    void updateTime()
+    {
+        remaningTime = timeTo;
+        displayTimer.text = timeTo.ToString();
+        
+    }
+
+    void countDownFinished()
+    {
+        if (remaningTime == 0)
+        {
+            StopCoroutine(TimerToDamage(0f));
+            textObject.SetActive(false);
+            
+        }
+    }
+    IEnumerator resetTimer(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        timeTo = 5;
+        updateTime();
+    }
+}
